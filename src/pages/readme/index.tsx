befce99e3a0e4a8120387e524754c7c3a5e330d0
solship <weldon48greenholtqnr@outlook.com>
2024-12-16 17:57:00 +0900

    Update index.tsx

--- src/pages/readme/index.tsx
+++ src/pages/readme/index.tsx
@@ -2,14 +2,14 @@
 const Welcome = () => {
     return (
         <>
-            <h1>Welcome to Solkeen's BotPad</h1>
-            <p>BotPad is Solkeen's Trading Bot consists of Sniping bot , Copytrading Bot , Volume Bot and so on.</p>
+            <h1>Welcome to Wizardev's BotPad</h1>
+            <p>BotPad is Wizardev's Trading Bot consists of Sniping bot , Copytrading Bot , Volume Bot and so on.</p>
             <p>This bot runs on several Solana dex like Pump.fun , Raydium , etc ..</p>
             <p>This Bot is beta version and other bots will be coming soon.</p>
             <p style={{marginBottom : '30px' , marginTop : '30px'}}>If you wanna get perfect bot , Contact me</p>
-            <p>Discord : <a href="https://discordapp.com/users/471524111512764447">! solkeen !</a></p>
-            <p>Telegram : <a href="https://t.me/soIkeen">! solkeen !</a></p>
-            <p>X.com : <a href="https://x.com/so1keen">@solkeen</a></p>
+            <p>Discord : <a href="https://discordapp.com/users/471524111512764447">! wizardev !</a></p>
+            <p>Telegram : <a href="https://t.me/wizardev">wizardev</a></p>
+            <p>X.com : <a href="https://x.com/_wizardev">@_wizardev</a></p>
         </>
     )
 }
